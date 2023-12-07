bf11949d1360f44d3a15fcfd8aa0148dbed97a32
Nik Divjak <nik.divjak@scheef.tech>
2023-12-07 13:42:52 +0100

    WIP

--- src/data_analyzer.py
+++ src/data_analyzer.py
@@ -1,5 +1,7 @@
+import math
 from typing import Dict, List, Union
 import numpy as np
+import pandas as pd
 
 from data_handler import DataHandler
 
@@ -8,61 +10,140 @@ class DataAnalyzer(DataHandler):
     def __init__(self, *args, **kwargs):
         # Call parent constructor
         super().__init__(*args, **kwargs)
+        self.train_set: pd.DataFrame = None
+        self.ideal_set: pd.DataFrame = None
+        self.best_fits: List[str] = None
 
+    def load_training_data(self, data):
+        self.train_set = data
+    
+    def load_ideal_data(self, data):
+        self.ideal_set = data
 
-    def mean_squared_error(self, y_true: np.array, y_pred: np.array) -> float | None:
+    def mean_squared_error(self, y_true: list, y_pred: list) -> float | None:
         if len(y_true) != len(y_pred):
             return
-        squared_diff = (y_true - y_pred) ** 2
-        mse = np.mean(squared_diff)
+        squared_diff = [(a - b) ** 2 for a, b in zip(y_true, y_pred)]
+        mse = sum(squared_diff) / len(squared_diff)
         return mse
 
 
-    def find_best_fit(self, train_set: np.ndarray, ideal_set: List[np.ndarray]) -> Dict[str, Union[str, np.array, float]] | None:
-        y_train = train_set[:, 1]  # Extract y values
-
-        # Initialize best curve and lowest MSE
-        best_curve = None
+    def find_best_fit(self, train_y: List):
+        if self.ideal_set is None:
+            print('Load ideal set first')
+            return
+        # Initialize curve_index and lowest MSE
+        curve_index = None
+        name = None
 
         # Set the lowest MSE to infinity so that the first curve will always be lower
         lowest_mse = float('inf')
 
         # Iterate through each curve in the ideal set
-        for i, curve in enumerate(ideal_set):
-            if len(curve) != len(train_set):
-                continue  # Skip this curve if it's not the same length as the train set
-
-            y_curve = curve[:, 1]  # Extract y values
-
+        for i in range(1, 51): 
+            # Extract y values from the ideal set
+            y_values =  self.ideal_set[f'y{i}'].tolist()
+          
             # Compute the MSE between the y values of the curve and the training set
-            mse = self.mean_squared_error(y_train, y_curve)
+            mse = self.mean_squared_error(train_y, y_values)
 
             # If the MSE is lower than the current lowest MSE, update the lowest MSE and best curve
             if mse < lowest_mse:
-                best_curve = curve
+                curve_index = i
                 lowest_mse = mse
-                # Index + 1 of the best curve
-                name = 'Func' + str(i + 1)
 
-        
-        if best_curve is None:
-            return None
-        return {'name': name, 'curve': best_curve, 'mse': lowest_mse}
+        if curve_index is not None:
+            name = f'y{curve_index}'
+
+        return name, lowest_mse    
     
-    def test_data_point(self, datapoint: np.array, best_fits: List[Dict[str, Union[str, np.array, float]]]) -> Dict[str, Union[str, float]] | None:
+    def find_ideal_curves(self):
+        if self.train_set is None:
+            print('Load training set first')
+            return
+        if self.ideal_set is None:
+            print('Load ideal set first')
+            return
+        best_fits: List[str] = []
+
+        # Iterate through each y column in the training set
+        for i in range(1, 5):
+            # Extract y values from the training set
+            train_y = self.train_set[f'y{i}'].tolist()
+            best_fit = self.find_best_fit(train_y)
+            # If best fit is found, append it to the best fits list
+            if best_fit[0] is not None:
+                best_fits.append(best_fit[0])
+        self.best_fits = best_fits
+        return best_fits
+    
+    def test_data_point(self, datapoint: List):
+        if self.best_fits is None:
+            print('Find best fits first')
+            return
+        if self.ideal_set is None:
+            print('Load ideal set first')
+            return
+
+        # Return values
         x = datapoint[0]
         y = datapoint[1]
-        result = None
+
+        # Set the deviation to infinity so that the first deviation will always be lower
+        rv_deviation = float('inf')
+        rv_ideal_func = None
+
+        # How do we get training_deviation?
+        training_deviation = 100
+
+        # Criteria for deviation
+        max_deviation = math.sqrt(2) * training_deviation
+
+        ideal = self.ideal_set
+        best_fits = self.best_fits
+        
+        # Iterate through each best fit
         for best_fit in best_fits:
-            max_deviation = best_fit['mse']
-            curve = best_fit['curve']
-            ideal_dict = dict(zip(curve[:, 0], curve[:, 1]))
-            if x in ideal_dict:
-             y_ideal = ideal_dict[x]
-             deviation = abs(y - y_ideal)
-             if deviation < max_deviation and (result is None or deviation < result['deviation']):
-                result = {'x': x, 'y': y, 'deviation': deviation, 'name': best_fit['name']}
-        if (result is None or result['deviation'] > np.sqrt(2)):
+            # Get y value from the pandas dataframe (ideal) based on the x value of the datapoint
+            ideal_y = ideal[ideal['x'] == x][best_fit].values[0]
+            # Compute the deviation
+            deviation = abs(y - ideal_y)
+            # If the deviation is lower or equal than the max deviation, set the ideal function and update the deviation
+            if deviation <= max_deviation and deviation < rv_deviation:
+                rv_deviation = deviation
+                rv_ideal_func = best_fit
+            
+        if (rv_ideal_func is None):
             return None
-        return result
+        return x, y, rv_deviation, rv_ideal_func
+        
+        
+    
+    def test_data_set(self, csv_path: str):
+        if self.best_fits is None:
+            print('Find best fits first')
+            return
+        if self.ideal_set is None:
+            print('Load ideal set first')
+            return
+        # Initialize empty list to store results
+        results = []
+
+        # Read the CSV file in chunks
+        chunks = pd.read_csv(csv_path, chunksize=1)
+
+        # Iterate through each chunk
+        for chunk in chunks:
+            # Extract x, y from chunk
+            x = chunk['x'].values[0]
+            y = chunk['y'].values[0]
+            datapoint = np.array([x, y])
+            # Call the function
+            result = self.test_data_point(datapoint)
+            if result is None:
+                print('No best fit found for datapoint ' + str(datapoint))
+            else:
+                results.append(result)
+        df = pd.DataFrame(results, columns=['x', 'y', 'delta_y', 'Ideal_Func'])
+        return df
     
--- src/main.py
+++ src/main.py
@@ -19,57 +19,26 @@ db_handler.create_table('ideal', table_ideal)
 db_handler.load_csv_to_db('data/train.csv', 'train')
 db_handler.load_csv_to_db('data/ideal.csv', 'ideal')
 
-# Initialize data analyzer
-db_analyzer = DataAnalyzer(db_handler)
+
 
 # Get data from tables
 train = db_handler.get_data_from_db('SELECT * FROM train')
 ideal = db_handler.get_data_from_db('SELECT * FROM ideal')
 
+# Initialize data analyzer
+db_analyzer = DataAnalyzer(db_handler)
 
+# Load data to data analyzer
+db_analyzer.load_training_data(train)
+db_analyzer.load_ideal_data(ideal)
 
-# Transform data into curves
-train_curves = db_handler.get_curves_from_frame(train)
-ideal_curves = db_handler.get_curves_from_frame(ideal)
+# Find best fits
+best_fits = db_analyzer.find_ideal_curves()
+# print(best_fits)
 
-# Set numpy print options (suppress scientific notation)
-np.set_printoptions(suppress=True)
+test_results = db_analyzer.test_data_set('data/test.csv')
+print(test_results)
 
-# Declare best fits array
-best_fits = []
+# Write test_results to database
+# db_analyzer.append_data_to_table('test', test_results)
 
-# Iterate through each curve in the training set
-for curve in train_curves:
-    best_fit = db_analyzer.find_best_fit(train_curves[0], ideal_curves)
-    best_fits.append(best_fit)
-    
-print(len(best_fits))
-
-
-chunks = pd.read_csv('data/test.csv', chunksize=1)
-
-test_data = pd.read_csv('data/test.csv')
-print (test_data['x'][0])
-
-# Example: Visualize the training data using a scatter plot
-# plt.figure(figsize=(8, 6))
-# plt.scatter(test_data['x'], test_data['y'], label='Test Data', color='blue')
-# plt.title('Training Data')
-# plt.xlabel('X')
-# plt.ylabel('Y')
-# plt.legend()
-# plt.show()
-
-
-
-for chunk in chunks:
-     # Extract x, y from chunk
-    x = chunk['x'].values[0]
-    y = chunk['y'].values[0]
-    datapoint = np.array([x, y])
-    # Call the function
-    result = db_analyzer.test_data_point(datapoint, best_fits)
-    if result is None:
-        print('No best fit found for datapoint ' + str(datapoint))
-    else:
-        print(result)
